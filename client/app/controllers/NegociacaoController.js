3262e38eb31f6e4c4442316fac8a2c40f7897f03
Gabriel <gabriel.martins091@hotmail.com>
2017-05-14 00:00:09 -0300

    NegociacoesView

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,12 +5,14 @@ class NegociacaoController {
 		this._inputqtd = $("#quantidade");
 		this._inputvalor = $("#valor");
 		this._listaNegociacoes = new ListaNegociacoes();
+		this._negociacoesView = new NegociacoesView($("#negociacoesView"));
+		this._negociacoesView.update(this._listaNegociacoes);
 	}
 
 	adiciona(e){
 		e.preventDefault();
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
-		console.log(this._listaNegociacoes.negociacoes);
+		this._negociacoesView.update(this._listaNegociacoes);
 		this._limpaForm();
 	}
 
